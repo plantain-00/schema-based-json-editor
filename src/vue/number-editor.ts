b5f72a0b21aec612e1966a6e66dfc406779c573d
york yao <yaoao12306@outlook.com>
2017-06-16 18:54:52 +0800

    shorter import

--- src/vue/number-editor.ts
+++ src/vue/number-editor.ts
@@ -1,4 +1,4 @@
-import * as Vue from "vue";
+import Vue from "vue";
 import Component from "vue-class-component";
 import * as common from "../common";
 import { srcVueNumberEditorTemplateHtml } from "../vue-variables";
