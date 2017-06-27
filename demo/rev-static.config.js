75e7080d0f348a918e44e5fb22e81cb57afea99f
york yao <yaoao12306@outlook.com>
2017-06-28 00:22:38 +0800

    improve web pack config

--- demo/rev-static.config.js
+++ demo/rev-static.config.js
@@ -1,17 +1,7 @@
 module.exports = {
     inputFiles: [
-        "demo/angular.js",
-        "demo/react.js",
-        "demo/vue.js",
-        "demo/angular.vendor.js",
-        "demo/react.vendor.js",
-        "demo/vue.vendor.js",
-        "demo/angular.editor.js",
-        "demo/react.editor.js",
-        "demo/vue.editor.js",
-        "demo/angular/index.ejs.html",
-        "demo/react/index.ejs.html",
-        "demo/vue/index.ejs.html"
+        "demo/**/*.bundle.js",
+        "demo/**/index.ejs.html",
     ],
     outputFiles: file => file.replace(".ejs", ""),
     json: false,
