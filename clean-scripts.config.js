0bdbf7c83c9a24bab7f181f1b11179e32b596cad
york yao <yaoao12306@outlook.com>
2017-08-28 20:38:54 +0800

    better screenshot

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -37,15 +37,17 @@ module.exports = {
       const { createServer } = require('http-server')
       const puppeteer = require('puppeteer')
       const fs = require('fs')
+      const beautify = require('js-beautify').html
       const server = createServer()
       server.listen(8000)
       const browser = await puppeteer.launch()
       const page = await browser.newPage()
+      await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
       for (const type of ['vue', 'react', 'angular']) {
         await page.goto(`http://localhost:8000/demo/${type}`)
         await page.screenshot({ path: `demo/${type}/screenshot.png`, fullPage: true })
         const content = await page.content()
-        fs.writeFileSync(`demo/${type}/screenshot-src.html`, content)
+        fs.writeFileSync(`demo/${type}/screenshot-src.html`, beautify(content))
       }
       server.close()
       browser.close()
