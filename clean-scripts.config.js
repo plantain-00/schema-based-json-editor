7205e2a6b557d1dc3d220b5e170c996bfd9c7b9e
york yao <yaoao12306@outlook.com>
2017-09-27 15:23:29 +0800

    improve script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -64,9 +64,7 @@ module.exports = {
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js',
-    'git checkout demo/vue/screenshot.png',
-    'git checkout demo/react/screenshot.png',
-    'git checkout demo/angular/screenshot.png',
+    'git checkout "demo/**/screenshot.png"',
     async () => {
       const { stdout } = await execAsync('git status -s')
       if (stdout) {
