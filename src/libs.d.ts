7edb0984097f37195054430fdac5f3cddd5d8533
york yao <yaoao12306@outlook.com>
2017-10-29 19:37:26 +0800

    fix CI

--- src/libs.d.ts
+++ src/libs.d.ts
@@ -1,4 +1,5 @@
 import * as dragula from "dragula";
+// tslint:disable:no-duplicate-imports
 import { MarkdownIt, Token, TokenRender, Renderer } from "markdown-it";
 import * as markdownit from "markdown-it";
 import * as hljs from "highlight.js";
