92841eef7a29582f62c62656c85584ad16deecc8
york yao <yaoao12306@outlook.com>
2017-11-21 17:35:01 +0800

    fix: react warning

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -23,7 +23,7 @@ const plugins = [
 const resolve = {
   alias: {
     'markdown-it': 'markdown-it/dist/markdown-it.min.js',
-    'vue$': 'vue/dist/vue.js'
+    'vue$': 'vue/dist/vue.min.js'
   }
 }
 
