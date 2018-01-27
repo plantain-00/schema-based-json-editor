57902f08ea560a0984075118501799a3ce18aae0
york yao <yaoao12306@outlook.com>
2018-01-27 22:11:08 +0800

    test: update screenshots

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -6,10 +6,10 @@ import * as puppeteer from 'puppeteer'
   await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
 
   const cases = [
-        { type: 'vue', url: '/packages/vue/demo' },
-        { type: 'react', url: '/packages/react/demo' },
-        { type: 'angular', url: '/packages/angular/demo/jit' },
-        { type: 'aot', url: '/packages/angular/demo/aot' }
+    { type: 'vue', url: '/packages/vue/demo' },
+    { type: 'react', url: '/packages/react/demo' },
+    { type: 'angular', url: '/packages/angular/demo/jit' },
+    { type: 'aot', url: '/packages/angular/demo/aot' }
   ]
 
   for (const { type, url } of cases) {
@@ -64,11 +64,11 @@ import * as puppeteer from 'puppeteer'
     await page.waitFor(100)
     await page.screenshot({ path: `screenshots/${type}-array.png` })
 
-    if (type === 'angular' || type === 'aot') {
-      await (page as any).select(`${prefix}:nth-child(100n+11) select`, 'enum 2')
-      await page.waitFor(100)
-      await page.screenshot({ path: `screenshots/${type}-enum.png` })
-    }
+    // if (type === 'angular' || type === 'aot') {
+    //   await (page as any).select(`${prefix}:nth-child(100n+11) select`, 'enum 2')
+    //   await page.waitFor(100)
+    //   await page.screenshot({ path: `screenshots/${type}-enum.png` })
+    // }
 
     await (page.type as any)(`${prefix}:nth-child(100n+16) textarea`, 'hello')
     await page.waitFor(100)
