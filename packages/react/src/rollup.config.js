04adb90ad394d7038b6c4434787f59fbf504b0e0
york yao <yaoao12306@outlook.com>
2018-06-03 07:49:00 +0800

    chore: update dependencies

--- packages/react/src/rollup.config.js
+++ packages/react/src/rollup.config.js
@@ -1,4 +1,4 @@
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 
