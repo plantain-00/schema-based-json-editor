9757aacab43447c77bab003e78454cf641413458
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-18 10:51:59 +0000

    Deploy plantain-00/schema-based-json-editor to github.com/plantain-00/schema-based-json-editor.git:gh-pages

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -55,7 +55,8 @@ module.exports = {
         `cpy ./packages/core/node_modules/monaco-editor/min/vs/editor/editor.main.css ./packages/core/demo/vs/editor/`,
         `cpy ./packages/core/node_modules/monaco-editor/min/vs/editor/editor.main.nls.js ./packages/core/demo/vs/editor/`,
         `cpy ./packages/core/node_modules/monaco-editor/min/vs/base/worker/workerMain.js ./packages/core/demo/vs/base/worker/`,
-        `cpy ./packages/core/node_modules/monaco-editor/min/vs/basic-languages/typescript/typescript.js ./packages/core/demo/vs/basic-languages/typescript/`
+        `cpy ./packages/core/node_modules/monaco-editor/min/vs/basic-languages/typescript/typescript.js ./packages/core/demo/vs/basic-languages/typescript/`,
+        `cpy ./packages/core/node_modules/monaco-editor/min/vs/basic-languages/javascript/javascript.js ./packages/core/demo/vs/basic-languages/javascript/`
       ],
       version: [
         {
