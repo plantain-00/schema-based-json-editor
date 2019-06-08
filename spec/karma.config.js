c5cec9448d4c31b59bfe366f62f91e358bb7fe65
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-08 12:29:03 +0000

    Deploy plantain-00/schema-based-json-editor to github.com/plantain-00/schema-based-json-editor.git:gh-pages

--- spec/karma.config.js
+++ spec/karma.config.js
@@ -18,6 +18,7 @@ module.exports = function (karma) {
     singleRun: true,
     concurrency: Infinity,
     webpack: webpackConfig,
+    webpackMiddleware: {},
     preprocessors: {
       '**/*Spec.js': ['webpack']
     }
